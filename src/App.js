8af935e6dbc01ad070ff127c8b18c35c2c0f8a46
Andrew <globalshow6@gmail.com>
2023-05-24 17:35:38 +0300

    Video 9 - Props func component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import Hello from "./components/Hello";
+import Greet from "./components/Greet";
 
 function App() {
   return (
     <div className="App">
-      <Hello/>
+      <Greet name="Андрей" heroName="Супермен">
+          <p>Это детский параметр</p>
+      </Greet>
+      <Greet name="Таня" heroName="Супер-Женщина">
+          <button>Действие</button>
+      </Greet>
+      <Greet name="Рома" heroName="Бетмен"/>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,4 +1,14 @@
 import React from "react";
 
-const Greet = () => <h1>Привет Андрей</h1>
+const Greet = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<h1>
+				Привет {props.name}, {props.heroName}
+			</h1>
+			{props.children}
+		</div>
+	)
+}
 export default Greet
