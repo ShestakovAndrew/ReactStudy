6e08e8ddbc5cd2632155661e20410c0edd8697fa
Andrew <globalshow6@gmail.com>
2023-05-24 18:46:18 +0300

    Video 14 - Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
 
 function App() {
   return (
     <div className="App">
-        <FunctionClick />
-        <ClassClick />
+        <EventBind />
     </div>
   );
 }
