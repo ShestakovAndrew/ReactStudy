40d4f96259e6385a4f9a1ab4968fecf343de67f9
Andrew <globalshow6@gmail.com>
2023-05-25 16:55:54 +0300

    Video 20 - Styling and CSS Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+
+import './appStyles.css'
+import styles from './appStyles.module.css';
 
 function App() {
   return (
     <div className="App">
-        <NameList />
+        <h1 className='error'> Error</h1>
+        <h1 className={styles.success}> Success</h1>
+
+        <Stylesheet primary={true} />
+        <Inline/>
     </div>
   );
 }
