12988979be2f6e3ffb039212f782041e116434a8
Andrew <globalshow6@gmail.com>
2023-05-26 14:14:34 +0300

    Video 33 - Higher Order Components (Part 1)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import Hero from "./components/Hero";
-import ErrorBoundary from "./components/ErrorBoundary";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 
 function App() {
   return (
     <div className="App">
-        <ErrorBoundary>
-            <Hero heroName='Бетмен' />
-        </ErrorBoundary>
-        <ErrorBoundary>
-            <Hero heroName='Супермен' />
-        </ErrorBoundary>
-        <ErrorBoundary>
-            <Hero heroName='Джокер' />
-        </ErrorBoundary>
+        <ClickCounter />
+        <HoverCounter />
     </div>
   );
 }
