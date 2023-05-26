00519de86580e51211b0350db9ac61e73641effe
Andrew <globalshow6@gmail.com>
2023-05-26 15:21:25 +0300

    Video 37 - Render Props (Part 2)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import ClickCounterTwo from "./components/ClickCounterTwo";
 import HoverCounterTwo from "./components/HoverCounterTwo";
+import Counter37 from "./components/Counter37";
 
 function App() {
   return (
     <div className="App">
-        <ClickCounterTwo />
-        <HoverCounterTwo />
+        <Counter37>
+            {(count, incrementCount) => (
+                <ClickCounterTwo count={count} incrementCount={incrementCount} />
+            )}
+        </Counter37>
+
+        <Counter37>
+            {(count, incrementCount) => (
+                <HoverCounterTwo count={count} incrementCount={incrementCount} />
+            )}
+        </Counter37>
     </div>
   );
 }
