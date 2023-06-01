c0c4fbdb99f0a2804947aa05a375e33827399056
Andrew <globalshow6@gmail.com>
2023-06-01 17:00:59 +0300

    React TypeScript Tutorial: 7 - Style Props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css';
-import {Status} from "./components/Status";
-import {Button} from "./components/Button";
-import {Input} from "./components/Input";
+import {Container} from "./components/Container";
 
 function App() {
     return (
     <div className="App">
-      <Status status='success' />
-        <Button handleClick={(event, id) => {
-            console.log('Button clicked', event, id)
-        }}
-        />
-        <Input value='' handleChange={event => console.log(event)} />
+        <Container styles={{border: '1px solid black', padding: '1rem'}}/>
     </div>
     )
 }
