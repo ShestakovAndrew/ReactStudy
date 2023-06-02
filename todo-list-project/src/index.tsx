8aba302f649edcff57c04c2d7ca61dd55f08e0b6
Andrew <globalshow6@gmail.com>
2023-06-02 15:11:07 +0300

    Добавил BrowserRouter

--- todo-list-project/src/index.tsx
+++ todo-list-project/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
 import store from "./app/store"
 
@@ -12,9 +14,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+      <BrowserRouter>
+          <Provider store={store}>
+              <App />
+          </Provider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
