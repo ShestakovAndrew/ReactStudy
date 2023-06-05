68fed67cb456692f4f8f880a2759df9680949880
Andrew <globalshow6@gmail.com>
2023-06-05 15:44:40 +0300

    Добавил навигацию по страницам

--- todo-list-project/src/App.tsx
+++ todo-list-project/src/App.tsx
@@ -3,15 +3,15 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 
 import {ViewTodoList} from "./components/view-todo-list-page/ViewTodoList";
-import {Home} from "./components/home-page/Home";
 import {CreateTodoElement} from "./components/new-todo-page/CreateTodoElement";
 import {NoMatch} from "./components/no-match-page/NoMatch";
+import {PageNavigationBar} from "./components/PageNavigationBar";
 
 function App() {
   return (
       <div>
+        <PageNavigationBar />
         <Routes>
-          <Route path='/' element={<Home />}/>
           <Route path='/view-todo-list' element={<ViewTodoList />}/>
           <Route path='/add-new-todo-element' element={<CreateTodoElement />}/>
           <Route path='*' element={<NoMatch />}/>
