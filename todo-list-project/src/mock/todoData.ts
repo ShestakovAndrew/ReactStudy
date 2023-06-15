30c96b5c15e7c5427d005c07483658c59ce64f5c
Andrew <globalshow6@gmail.com>
2023-06-15 17:36:36 +0300

    перенёс стартовые данные

---
+++ todo-list-project/src/mock/todoData.ts
@@ -0,0 +1,46 @@
+import {TodoElementType, TodoPriorityEnum} from "./todoSlice.types";
+
+export const todoList: TodoElementType[] = [
+    {
+        id: '0',
+        title: 'Проснуться',
+        description: "",
+        priority: TodoPriorityEnum.Low
+    },
+    {
+        id: "1",
+        title: "Встать",
+        description: "Хорошо бы до 12 дня",
+        priority: TodoPriorityEnum.Low
+    },
+    {
+        id: "2",
+        title: "Покушать",
+        description: "пресс-пицца мясной бум конечно",
+        priority: TodoPriorityEnum.High
+    },
+    {
+        id: "3",
+        title: "Почистить зубы",
+        description: "утром и вечером",
+        priority: TodoPriorityEnum.Low
+    },
+    {
+        id: "4",
+        title: "Погулять",
+        description: "негде гулять особо",
+        priority: TodoPriorityEnum.Low
+    },
+    {
+        id: "5",
+        title: "Поиграть в minecraft",
+        description: "ежедневная доза",
+        priority: TodoPriorityEnum.High
+    },
+    {
+        id: "6",
+        title: "Покататься на самокате",
+        description: "до yolka в кино и обратно",
+        priority: TodoPriorityEnum.Medium
+    }
+]
--- todo-list-project/src/redux/todoSlice.ts
+++ todo-list-project/src/redux/todoSlice.ts
@@ -1,55 +1,13 @@
 import {createSlice} from '@reduxjs/toolkit'
-import {TodoElementType, TodoPriorityEnum} from "../mock/todoSlice.types";
+import {TodoElementType} from "../mock/todoSlice.types";
+import {todoList} from "../mock/todoData";
 
 type InitialStateType = {
     todoList: TodoElementType[]
 }
 
 const initialState: InitialStateType = {
-    todoList: [
-        {
-            id: '0',
-            title: 'Проснуться',
-            description: "",
-            priority: TodoPriorityEnum.Low
-        },
-        {
-            id: "1",
-            title: "Встать",
-            description: "Хорошо бы до 12 дня",
-            priority: TodoPriorityEnum.Low
-        },
-        {
-            id: "2",
-            title: "Покушать",
-            description: "пресс-пицца мясной бум конечно",
-            priority: TodoPriorityEnum.High
-        },
-        {
-            id: "3",
-            title: "Почистить зубы",
-            description: "утром и вечером",
-            priority: TodoPriorityEnum.Low
-        },
-        {
-            id: "4",
-            title: "Погулять",
-            description: "негде гулять особо",
-            priority: TodoPriorityEnum.Low
-        },
-        {
-            id: "5",
-            title: "Поиграть в minecraft",
-            description: "ежедневная доза",
-            priority: TodoPriorityEnum.High
-        },
-        {
-            id: "6",
-            title: "Покататься на самокате",
-            description: "до yolka в кино и обратно",
-            priority: TodoPriorityEnum.Medium
-        }
-    ]
+    todoList: todoList
 }
 
 const todoSlice = createSlice({
