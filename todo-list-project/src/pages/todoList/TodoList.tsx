07040052918651dc25cd359821d66955694539d5
Andrew <globalshow6@gmail.com>
2023-06-16 19:44:15 +0300

    обновил отображение задач, убрал тег table

--- todo-list-project/src/pages/todoList/TodoList.tsx
+++ todo-list-project/src/pages/todoList/TodoList.tsx
@@ -1,29 +1,24 @@
 import {useTodoListSelector} from "../../redux/hooks/useTodoListSelector";
+import {TodoElementType} from "../../mock/todo.types";
+import {TodoListItem} from "./todoListItem/TodoListItem";
 
 export const TodoList = () => {
-    const todoList = useTodoListSelector()
+    const todoList: TodoElementType[] = useTodoListSelector()
 
     return (
         <div>
-            <h2>Список дел</h2>
-            <table className='todoTable'>
-                <thead>
-                <tr>
-                    <th>Название</th>
-                    <th>Описание</th>
-                    <th>Приоритет</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {todoList.map(todoElement => (
-                        <tr key={todoElement.id}>
-                            <td>{todoElement.title}</td>
-                            <td>{todoElement.description}</td>
-                            <td>{todoElement.priority}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <h2>Мой список дел</h2>
+            {
+                todoList.map(todoItem => (
+                    <TodoListItem
+                        title={todoItem.title}
+                        description={todoItem.description}
+                        priority={todoItem.priority}
+                    />
+                    )
+                )
+            }
+            <button>Добавить новое дело</button>
         </div>
     )
 }
--- todo-list-project/src/pages/todoList/todoListItem/TodoListItem.tsx
+++ todo-list-project/src/pages/todoList/todoListItem/TodoListItem.tsx
@@ -1,3 +1,18 @@
-export const TodoListItem = () => {
-    return <div>NavigationBar</div>
+import {TodoPriorityEnum} from "../../../mock/todo.types";
+
+type TodoElementTypeProps = {
+    title: string,
+    description: string,
+    priority: TodoPriorityEnum
+}
+
+
+export const TodoListItem = ({title, description, priority}: TodoElementTypeProps) => {
+    return (
+        <ul>
+            <li>{title}</li>
+            <li>{description}</li>
+            <li>{priority}</li>
+        </ul>
+    )
 }
