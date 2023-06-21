60564d64c0b2a01f2594ad51fccc823bac01336c
Andrew <globalshow6@gmail.com>
2023-06-21 20:22:22 +0300

    Добавил css для заголовка

--- todo-list-project/src/pages/todoList/TodoList.tsx
+++ todo-list-project/src/pages/todoList/TodoList.tsx
@@ -1,12 +1,13 @@
 import {useTodoListSelector} from "../../redux/hooks/useTodoListSelector"
 import {TodoItem} from "./todoItem/TodoItem"
+import styles from "./TodoList.module.css"
 
 const TodoList = () => {
     const todoList = useTodoListSelector()
 
     return (
         <div>
-            <h1>Мой список дел</h1>
+            <h1 className={styles.header}>Мой список дел</h1>
             {
                 todoList.map(todoItem => (
                     <TodoItem key={todoItem.id}
@@ -14,8 +15,7 @@ const TodoList = () => {
                         description={todoItem.description}
                         priority={todoItem.priority}
                     />
-                    )
-                )
+                ))
             }
             <button>Добавить новое дело</button>
         </div>
