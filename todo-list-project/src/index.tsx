6fd17cdff0e5dc76f5a1f8a23fff53b3219b4a57
Andrew <globalshow6@gmail.com>
2023-06-23 10:26:31 +0300

    Удалил лишние в проекте файлы

--- todo-list-project/src/index.tsx
+++ todo-list-project/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
@@ -21,5 +20,3 @@ root.render(
       </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
