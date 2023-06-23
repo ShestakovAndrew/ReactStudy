da9583b9d5b08e9dec40308eccca6b463faa192b
Andrew <globalshow6@gmail.com>
2023-06-23 18:41:57 +0300

    Добавил перенаправление на страницу создания todo item

--- todo-list-project/src/pages/createTodoItem/CreateTodoItem.tsx
+++ todo-list-project/src/pages/createTodoItem/CreateTodoItem.tsx
@@ -1,5 +1,24 @@
+import React from "react"
+
 const CreateTodoItem = () => {
-    return <div>CreateTodoElement Page</div>
+    return (
+        <form>
+            <div className="mb-3">
+                <label className="form-label">Название</label>
+                <input type="text" className="form-control" placeholder="Название задачи"/>
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Описание</label>
+                <input type="text" className="form-control" placeholder="Описание задачи"/>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <button className='btn btn-success'>
+                    Сохранить
+                </button>
+            </div>
+        </form>
+
+    )
 }
 
 export {
--- todo-list-project/src/pages/todoList/TodoList.tsx
+++ todo-list-project/src/pages/todoList/TodoList.tsx
@@ -1,12 +1,16 @@
 import {useTodoListSelector} from "../../redux/hooks/useTodoListSelector"
-import {TodoItem} from "./todoItem/TodoItem"
-import styles from "./TodoList.module.css"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {NavigationBar, PAGE_FIRST, RECORDS_PER_PAGE} from "./navigationBar/NavigationBar"
 import {TodoItemData} from "./model/todo.types"
 import React, {useState} from "react"
+import {useNavigate} from "react-router-dom"
+
+import styles from "./TodoList.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {TodoItem} from "./todoItem/TodoItem"
+import {NavigationBar, PAGE_FIRST, RECORDS_PER_PAGE} from "./navigationBar/NavigationBar"
 
 const TodoList = () => {
+    const navigate = useNavigate()
     const todoList = useTodoListSelector()
     const [currentPage, setCurrentPage] = useState(PAGE_FIRST)
 
@@ -20,9 +24,11 @@ const TodoList = () => {
 
             <table className='table table-striped table-sm'>
                 <thead>
-                    <th>Заголовок</th>
-                    <th>Описание</th>
-                    <th>Приоритет</th>
+                    <tr>
+                        <th>Заголовок</th>
+                        <th>Описание</th>
+                        <th>Приоритет</th>
+                    </tr>
                 </thead>
                 <tbody>
                 {
@@ -30,17 +36,21 @@ const TodoList = () => {
                         <TodoItem key={todoItem.id}
                                   title={todoItem.title}
                                   description={todoItem.description}
-                                  priority={todoItem.priority}
-                        />
+                                  priority={todoItem.priority}/>
                     ))
                 }
                 </tbody>
             </table>
 
-            <NavigationBar currentPage={currentPage} setCurrentPage={setCurrentPage} todoListLength={todoList.length} />
+            <NavigationBar
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                todoListLength={todoList.length}/>
 
             <div className='d-flex justify-content-center'>
-                <button className='btn btn-success'>Добавить новое дело</button>
+                <button className='btn btn-success' onClick={() => navigate('/create-todo-item')}>
+                    Добавить новое дело
+                </button>
             </div>
         </div>
     )
