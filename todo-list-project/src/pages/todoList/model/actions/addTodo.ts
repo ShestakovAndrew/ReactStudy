14355763f9880de82b3b35e7dc8792cba621765b
Andrew <globalshow6@gmail.com>
2023-07-10 09:31:59 +0300

    Упростил addTodo

--- todo-list-project/src/pages/todoList/model/actions/addTodo.ts
+++ todo-list-project/src/pages/todoList/model/actions/addTodo.ts
@@ -1,16 +1,14 @@
 import {Draft, PayloadAction} from "@reduxjs/toolkit"
-import {TodoItemData, TodoItemProps} from "../todo.types"
+import {TodoItemProps} from "../todo.types"
 import {TodoListState} from "../todoSlice"
 
 const addTodo = (state: Draft<TodoListState>, action: PayloadAction<TodoItemProps>) => {
-    const newTodo: TodoItemData = {
+    state.todoList.push({
         id: Date.now().toString(),
         title: action.payload.title,
         description: action.payload.description,
         priority: action.payload.priority,
-    }
-
-    state.todoList.push(newTodo)
+    })
 }
 
 export {
