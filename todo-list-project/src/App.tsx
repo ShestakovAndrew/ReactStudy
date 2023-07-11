4ed3048db2003e21039440961a76b6fa30bacad8
Andrew <globalshow6@gmail.com>
2023-07-11 12:20:57 +0300

    заменил на табы

--- todo-list-project/src/App.tsx
+++ todo-list-project/src/App.tsx
@@ -5,15 +5,15 @@ import {TodoList} from "./pages/todoList/TodoList"
 import {CreateTodoItem} from "./pages/createTodoItem/CreateTodoItem"
 
 function App() {
-  return (
-      <div>
-        <Routes>
-          <Route path='/todo-list' element={<TodoList />}/>
-          <Route path='/create-todo-item' element={<CreateTodoItem />}/>
-          <Route path='*' element={<Navigate to='/todo-list' replace />}/>
-        </Routes>
-      </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path='/todo-list' element={<TodoList />}/>
+                <Route path='/create-todo-item' element={<CreateTodoItem />}/>
+                <Route path='*' element={<Navigate to='/todo-list' replace />}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default App
